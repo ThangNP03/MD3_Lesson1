00bd4c14f95fda3caa40c75714dd1e6dd98e7ad2
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-03-07 11:45:54 +0700

    LS1

--- src/Main2.java
+++ src/Main2.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
-
+        Scanner numberInput = new Scanner(System.in);
+        System.out.println("Mời nhập vào số tiền  muốn quy đổi: ");
+        short number = numberInput.nextShort();
+        System.out.println("số tiền quy đổi được bằng " + number * 23000 + " vnd");
     }
 }
